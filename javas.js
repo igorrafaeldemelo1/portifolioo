9f8968ce0935e64906daf914f959a22549d676a2
igorrafaeldemelo1 <86748756+igorrafaeldemelo1@users.noreply.github.com>
2022-11-21 21:41:33 -0300

    att2

---
+++ javas.js
@@ -0,0 +1,8 @@
+function projeto01() {
+    let modal = document.querySelector('.projeto-01')
+    modal.style.display = 'block';
+}
+function close01(){
+    let close = document.querySelector('.projeto-01')
+    close.style.display = 'none';
+}

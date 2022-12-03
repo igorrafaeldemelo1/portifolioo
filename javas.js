22bd67ebce391f72dbe2770e33adc05ed4812427
igorrafaeldemelo1 <86748756+igorrafaeldemelo1@users.noreply.github.com>
2022-12-03 19:42:06 -0300

    att 3

--- javas.js
+++ javas.js
@@ -1,17 +1,24 @@
 function projeto01() {
-    let modal = document.querySelector('.projeto-01')
+    const modal = document.querySelector('.projeto-01')
     modal.style.display = 'block';
 }
 function close01(){
     let close = document.querySelector('.projeto-01')
     close.style.display = 'none';
 }
-
 function projeto02() {
     let modal = document.querySelector('.projeto-02')
     modal.style.display = 'block';
 }
 function close02(){
     let close = document.querySelector('.projeto-02')
+    close.style.display = 'none';
+}
+function projeto03() {
+    let modal = document.querySelector('.projeto-03')
+    modal.style.display = 'block';
+}
+function close03(){
+    let close = document.querySelector('.projeto-03')
     close.style.display = 'none';
 }
